6c7a93f5583c8fa5348727b647b4063654784279
Vidmantas Burba <vidmantasburba@Vidmantass-Air.lan>
2020-04-25 19:43:20 +0300

    Add stroke gauge

--- app.js
+++ app.js
@@ -196,6 +196,55 @@ window.onload = function () {
     },
     ],
   });
-  chart2.render();
+
+  const options = {
+    series: [67],
+    chart: {
+      height: 350,
+      type: 'radialBar',
+      offsetY: -10,
+    },
+    plotOptions: {
+      radialBar: {
+        startAngle: -135,
+        endAngle: 135,
+        dataLabels: {
+          name: {
+            fontSize: '16px',
+            color: undefined,
+            offsetY: 120,
+          },
+          value: {
+            offsetY: 76,
+            fontSize: '22px',
+            color: undefined,
+            formatter(val) {
+              return `${val}%`;
+            },
+          },
+        },
+      },
+    },
+    fill: {
+      type: 'gradient',
+      gradient: {
+        shade: 'dark',
+        shadeIntensity: 0.15,
+        inverseColors: false,
+        opacityFrom: 1,
+        opacityTo: 1,
+        stops: [0, 50, 65, 91],
+      },
+    },
+    stroke: {
+      dashArray: 4,
+    },
+    labels: ['Median Ratio'],
+  };
+
+  const chart3 = new ApexCharts(document.querySelector('#chart3'), options);
+
   chart1.render();
+  chart2.render();
+  chart3.render();
 };
